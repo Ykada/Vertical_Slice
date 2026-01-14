fb1b10056690305f14bdc0c04a1ddd8c5943c116
Yk_ <bo.bolweg@gmail.com>
2026-01-14 14:26:03 +0100

    a

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -40,7 +40,7 @@ public class MainMenuUI : MonoBehaviour
         {
             CampainstartCanvas.SetActive(false);
             savingCanvas.SetActive(true);
-            Backgroundimage.transform.position = new Vector3(1955, 650, 0);
+            Backgroundimage.transform.position = new Vector3(1955, 950, 0);
             othertransforms[0].transform.position = new Vector3(1955, 650, 0);
             othertransforms[1].transform.position = new Vector3(1955, 0, 0);
             othertransforms[2].transform.position = new Vector3(1955, -50, 0);
